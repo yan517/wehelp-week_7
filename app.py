eed1dbb8aa69588b2a1350de54514c588ea329af
Yan <yan960517@gmail.com>
2022-11-03 12:10:25 +0800

    updated

--- app.py
+++ app.py
@@ -24,26 +24,24 @@ def register():
         try:
             # Get connection object from a pool
             connection_object = connection_pool.get_connection()
-            if connection_object.is_connected():
-                cursor = connection_object.cursor()
-                check_user_exist = ("SELECT * FROM member WHERE username = %s;")
-                cursor.execute(check_user_exist,(username,))
-                if (cursor.fetchone() is not None):
-                    return redirect(url_for("error", message="帳號已經被註冊"))
-                else:
-                    create_user = ("INSERT INTO member "
-                                    "(name, username, password)"
-                                    "VALUES (%s, %s, %s);")  
-                    cursor.execute(create_user,(name,username,pwd))
-                    connection_object.commit()
-                    return redirect(url_for("index"))
+            cursor = connection_object.cursor()
+            check_user_exist = ("SELECT * FROM member WHERE username = %s;")
+            cursor.execute(check_user_exist,(username,))
+            if (cursor.fetchone() is not None):
+                return redirect(url_for("error", message="帳號已經被註冊"))
+            else:
+                create_user = ("INSERT INTO member "
+                                "(name, username, password)"
+                                "VALUES (%s, %s, %s);")  
+                cursor.execute(create_user,(name,username,pwd))
+                connection_object.commit()
+                return redirect(url_for("index"))
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
         finally:
             # closing database connection.
-            if connection_object.is_connected():
-                cursor.close()
-                connection_object.close()
+            cursor.close()
+            connection_object.close()
     else:
         return redirect(url_for("error", message="姓名、帳號、密碼不能空"))
 
@@ -54,24 +52,22 @@ def signIn():
     if(usrname and passwd):
         try:
             connection_object = connection_pool.get_connection()
-            if connection_object.is_connected():
-                cursor = connection_object.cursor()
-                check_user_passwd = ("SELECT id, name, follower_count FROM member WHERE BINARY username = %s and BINARY password = %s;")
-                cursor.execute(check_user_passwd,(usrname,passwd))
-                fetchdata = cursor.fetchone()
-                if (fetchdata is not None):
-                    session["login"] = "success"
-                    session["userProfile"] = list(fetchdata)
-                    return redirect("/member")
-                else:
-                    session["login"] = "fail"
-                    return redirect(url_for("error", message="帳號、或密碼輸入錯誤"))
+            cursor = connection_object.cursor()
+            check_user_passwd = ("SELECT id, name, follower_count FROM member WHERE BINARY username = %s and BINARY password = %s;")
+            cursor.execute(check_user_passwd,(usrname,passwd))
+            fetchdata = cursor.fetchone()
+            if (fetchdata is not None):
+                session["login"] = "success"
+                session["userProfile"] = list(fetchdata)
+                return redirect("/member")
+            else:
+                session["login"] = "fail"
+                return redirect(url_for("error", message="帳號、或密碼輸入錯誤"))
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
         finally:
-            if connection_object.is_connected():
-                cursor.close()
-                connection_object.close()
+            cursor.close()
+            connection_object.close()
 
     else:
         session["login"] = "fail"
@@ -102,20 +98,18 @@ def addCom():
     if(comment):
         try:
             connection_object = connection_pool.get_connection()
-            if connection_object.is_connected():
-                cursor = connection_object.cursor()
-                add_comment = ("INSERT INTO message "
-                            "(member_id, content)"
-                            "VALUES (%s, %s);")
-                cursor.execute(add_comment,(session["userProfile"][0],comment))
-                connection_object.commit() 
-                return redirect("/member")
+            cursor = connection_object.cursor()
+            add_comment = ("INSERT INTO message "
+                        "(member_id, content)"
+                        "VALUES (%s, %s);")
+            cursor.execute(add_comment,(session["userProfile"][0],comment))
+            connection_object.commit() 
+            return redirect("/member")
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
         finally:
-            if connection_object.is_connected():
-                cursor.close()
-                connection_object.close()
+            cursor.close()
+            connection_object.close()
     else:
         return redirect("/member")
 
@@ -123,18 +117,16 @@ def getData():
     if (session["login"] == "success"):
         try:
             connection_object = connection_pool.get_connection()
-            if connection_object.is_connected():
-                cursor = connection_object.cursor()
-                get_commemt = ("SELECT name, content, message.time from message left join member on member.id = message.member_id order by message.time DESC;")
-                cursor.execute(get_commemt)
-                data = cursor.fetchall()
-                return data
+            cursor = connection_object.cursor()
+            get_commemt = ("SELECT name, content, message.time from message left join member on member.id = message.member_id order by message.time DESC;")
+            cursor.execute(get_commemt)
+            data = cursor.fetchall()
+            return data
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
         finally:
-            if connection_object.is_connected():
-                cursor.close()
-                connection_object.close()   
+            cursor.close()
+            connection_object.close()   
 
 # API
 @app.route("/api/member", methods=["GET"])
@@ -143,24 +135,22 @@ def apiMember():
         try:
                 username = request.args['username']
                 connection_object = connection_pool.get_connection()
-                if connection_object.is_connected():
-                    cursor = connection_object.cursor()
-                    get_profile = ("SELECT id, name, username FROM member WHERE username = %s;")
-                    cursor.execute(get_profile,(username,))
-                    data = cursor.fetchone()
-                    if data:
-                        profile = { 
-                                "id": data[0],
-                                "name": data[1],
-                                "username" : data[2]
-                        }
-                        return jsonify({"data":profile})
-                    else:
-                        return jsonify({"data":None})
+                cursor = connection_object.cursor()
+                get_profile = ("SELECT id, name, username FROM member WHERE username = %s;")
+                cursor.execute(get_profile,(username,))
+                data = cursor.fetchone()
+                if data:
+                    profile = { 
+                            "id": data[0],
+                            "name": data[1],
+                            "username" : data[2]
+                    }
+                    return jsonify({"data":profile})
+                else:
+                    return jsonify({"data":None})
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
         finally:
-            if connection_object.is_connected():
                 cursor.close()
                 connection_object.close()   
     else:
@@ -172,29 +162,27 @@ def apiUpdateMember():
     if (req['name'] and session["login"] == "success"):
         try:
             connection_object = connection_pool.get_connection()
-            if connection_object.is_connected():
-                cursor = connection_object.cursor()
-                check_user_exist = ("SELECT * FROM member WHERE name = %s;")
-                cursor.execute(check_user_exist,(req['name'],))
-                if (cursor.fetchone() is not None):
-                    return make_response(jsonify({"error":"true"}),404)
-                else:
-                    update_name = ("UPDATE member "
-                                   "SET name = %s"
-                                    "WHERE name = %s;")  
-                    cursor.execute(update_name,(req['name'],session["userProfile"][1]))
-                    connection_object.commit()
-                    temp = (session["userProfile"][0],req['name'],session["userProfile"][2])
-                    session.pop('userProfile', default=None)
-                    session['userProfile'] = list(temp)
-                    return make_response(jsonify({"ok":"true"}),200)
+            cursor = connection_object.cursor()
+            check_user_exist = ("SELECT * FROM member WHERE name = %s;")
+            cursor.execute(check_user_exist,(req['name'],))
+            if (cursor.fetchone() is not None):
+                return make_response(jsonify({"error":"true"}),404)
+            else:
+                update_name = ("UPDATE member "
+                                "SET name = %s"
+                                "WHERE id = %s;")  
+                cursor.execute(update_name,(req['name'],session["userProfile"][0]))
+                connection_object.commit()
+                temp = (session["userProfile"][0],req['name'],session["userProfile"][2])
+                session.pop('userProfile', default=None)
+                session['userProfile'] = list(temp)
+                return make_response(jsonify({"ok":"true"}),200)
         except Error as e:
             print("Error while connecting to MySQL using Connection pool ", e)
             return make_response(jsonify({"error":"true"}),404)
         finally:
-            if connection_object.is_connected():
-                cursor.close()
-                connection_object.close()
+            cursor.close()
+            connection_object.close()
     else:
         return make_response(jsonify({"error":"true"}),404)
 
